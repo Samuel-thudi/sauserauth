709e705c2b6e292385a914c6f91459417707a02a
Samuel Thudi <samuel.thudi@gmail.com>
2023-12-05 14:57:51 +0530

    new bot

--- dialogs/main_dialog.py
+++ dialogs/main_dialog.py
@@ -1,19 +1,16 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-from botbuilder.core import MessageFactory, CardFactory
-from botbuilder.dialogs import WaterfallDialog, WaterfallStepContext, DialogTurnResult
-from botbuilder.dialogs.prompts import (
-    OAuthPrompt,
-    OAuthPromptSettings,
-    ConfirmPrompt,
+from botbuilder.core import MessageFactory
+from botbuilder.dialogs import (
+    WaterfallDialog,
+    WaterfallStepContext,
+    DialogTurnResult,
     PromptOptions,
-    TextPrompt,
 )
-from botbuilder.schema import HeroCard, CardImage
+from botbuilder.dialogs.prompts import OAuthPrompt, OAuthPromptSettings, ConfirmPrompt
 
 from dialogs import LogoutDialog
-from simple_graph_client import SimpleGraphClient
 
 
 class MainDialog(LogoutDialog):
@@ -32,7 +29,6 @@ class MainDialog(LogoutDialog):
             )
         )
 
-        self.add_dialog(TextPrompt(TextPrompt.__name__))
         self.add_dialog(ConfirmPrompt(ConfirmPrompt.__name__))
 
         self.add_dialog(
@@ -41,8 +37,8 @@ class MainDialog(LogoutDialog):
                 [
                     self.prompt_step,
                     self.login_step,
-                    self.command_step,
-                    self.process_step,
+                    self.display_token_phase1,
+                    self.display_token_phase2,
                 ],
             )
         )
@@ -58,11 +54,9 @@ class MainDialog(LogoutDialog):
         if step_context.result:
             await step_context.context.send_activity("You are now logged in.")
             return await step_context.prompt(
-                TextPrompt.__name__,
+                ConfirmPrompt.__name__,
                 PromptOptions(
-                    prompt=MessageFactory.text(
-                        "Would you like to do? (type 'me' or 'email')"
-                    )
+                    prompt=MessageFactory.text("Would you like to view your token?")
                 ),
             )
 
@@ -71,52 +65,30 @@ class MainDialog(LogoutDialog):
         )
         return await step_context.end_dialog()
 
-    async def command_step(
+    async def display_token_phase1(
         self, step_context: WaterfallStepContext
     ) -> DialogTurnResult:
-        step_context.values["command"] = step_context.result
+        await step_context.context.send_activity("Thank you.")
 
-        # Call the prompt again because we need the token. The reasons for this are:
-        # 1. If the user is already logged in we do not need to store the token locally in the bot and worry
-        #    about refreshing it. We can always just call the prompt again to get the token.
-        # 2. We never know how long it will take a user to respond. By the time the
-        #    user responds the token may have expired. The user would then be prompted to login again.
-        #
-        # There is no reason to store the token locally in the bot because we can always just call
-        # the OAuth prompt to get the token or get a new token if needed.
-        return await step_context.begin_dialog(OAuthPrompt.__name__)
+        if step_context.result:
+            # Call the prompt again because we need the token. The reasons for this are:
+            # 1. If the user is already logged in we do not need to store the token locally in the bot and worry
+            #    about refreshing it. We can always just call the prompt again to get the token.
+            # 2. We never know how long it will take a user to respond. By the time the
+            #    user responds the token may have expired. The user would then be prompted to login again.
+            #
+            # There is no reason to store the token locally in the bot because we can always just call
+            # the OAuth prompt to get the token or get a new token if needed.
+            return await step_context.begin_dialog(OAuthPrompt.__name__)
 
-    async def process_step(
+        return await step_context.end_dialog()
+
+    async def display_token_phase2(
         self, step_context: WaterfallStepContext
     ) -> DialogTurnResult:
         if step_context.result:
-            token_response = step_context.result
-            if token_response and token_response.token:
-                parts = step_context.values["command"].split(" ")
-                command = parts[0]
+            await step_context.context.send_activity(
+                f"Here is your token {step_context.result.token}"
+            )
 
-                # display logged in users name
-                if command == "me":
-                    client = SimpleGraphClient(token_response.token)
-                    me_info = await client.get_me()
-                    await step_context.context.send_activity(
-                        f"You are {me_info['displayName']}"
-                    )
-
-                # display logged in users email
-                elif command == "email":
-                    client = SimpleGraphClient(token_response.token)
-                    me_info = await client.get_me()
-                    await step_context.context.send_activity(
-                        f"Your email: {me_info['mail']}"
-                    )
-
-                else:
-                    await step_context.context.send_activity(
-                        f"Your token is {token_response.token}"
-                    )
-        else:
-            await step_context.context.send_activity("We couldn't log you in.")
-
-        await step_context.context.send_activity("Type anything to try again.")
         return await step_context.end_dialog()
