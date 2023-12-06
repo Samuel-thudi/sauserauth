032346927b37c82a1d3813510de9b043774ed54e
Samuel Thudi <samuel.thudi@gmail.com>
2023-12-06 11:14:51 +0530

    updated config

--- config.py
+++ config.py
@@ -11,6 +11,6 @@ class DefaultConfig:
     """ Bot Configuration """
 
     PORT = 3978
-    APP_ID = os.environ.get("MicrosoftAppId", "")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
-    CONNECTION_NAME = os.environ.get("ConnectionName", "")
+    APP_ID = os.environ.get("MicrosoftAppId", "78197bf0-f9d8-4fea-a270-c2c7e728f3e8")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "MAk8Q~XDthrZLqp-eFcR8KzBrMcuKS.Ni32uXduK")
+    CONNECTION_NAME = os.environ.get("ConnectionName", "authentication-conn")
